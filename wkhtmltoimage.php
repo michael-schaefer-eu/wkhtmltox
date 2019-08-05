8775d21ee175edf3c0e465cdf1fc7010b53b9e69
Michael Schaefer <mail@michael-schaefer.eu>
2019-08-05 15:46:23 +0200

    fix typo in const names

--- wkhtmltoimage.php
+++ wkhtmltoimage.php
@@ -3,7 +3,7 @@ namespace Wkhtmltox;
 
 class Wkhtmltoimage
 {
-    const BIONIC-AMD64 = __DIR__ . '/bin/wkhtmltoimage-bionic-amd64';
-    const BIONIC-I386 = __DIR__ . '/bin/wkhtmltoimage-bionic-i386';
+    const BIONICAMD64 = __DIR__ . '/bin/wkhtmltoimage-bionic-amd64';
+    const BIONICI386 = __DIR__ . '/bin/wkhtmltoimage-bionic-i386';
 }
 
